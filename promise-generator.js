b9cc3bef3cddee4adbcd08c3ade26c248fc2e121
Raysuner <1669232805@qq.com>
2021-09-18 19:47:51 +0800

    update promise within generator

--- promise-generator.js
+++ promise-generator.js
@@ -1,39 +1,57 @@
-function getValue(n, time = 1000) {
+function getValue(n, ms = 1000) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(n)
-        }, time)
+        }, ms)
     })
 }
 
 function * generator() {
     console.log(1)
-    let x = yield getValue(2)
-    console.log(3)
-    x = yield getValue(4)
+    let x = yield getValue(2, 1000)
+    console.log(x)
+    x = yield getValue(3, 2000)
+    console.log(x)
+    return 4
 }
 
 
 function run(generator) {
-    let iterable = generator()
-    let gene = iterable.next()
-    function tick() {
-        if (gene.done === false) {
-            gene.value.then(
-                val => {
-                    console.log(val);
-                    gene = iterable.next(val)
-                    tick()
-                },
-                reason => {
-                    console.log(reason.message)
-                    gene = iterable.throw(reason)
-                    tick()
-                }
-            )
+    return new Promise((resolve, reject) => {
+        let iterable = generator()
+        let gene = iterable.next()
+        tick()
+
+        function tick() {
+            if (gene.done === false) {
+                gene.value.then(
+                    value => {
+                        try {
+                            gene = iterable.next(value)
+                            tick()
+                        } catch (err) {
+                            reject(err)
+                        }
+                    },
+                    reason => {
+                        try {
+                            gene = iterable.throw(reason)
+                            tick()
+                        }
+                        catch (err) {
+                            reject(err)
+                        }
+                    }
+                )
+            }
+            else {
+                resolve(gene.value)
+            }
         }
-    }
-    tick() // 第一次就算是传值也没有作用
+    })
 }
 
-run(generator)
+run(generator).then((val) => {
+    console.log(val)
+    console.log('generator is done');
+})
